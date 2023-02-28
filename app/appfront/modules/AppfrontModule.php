c3264396f1c7592f88e8376ca18752a179b9856b
laijinyou <1183820@qq.com>
2023-02-28 09:10:39 +0900

    module

--- app/appfront/modules/AppfrontModule.php
+++ app/appfront/modules/AppfrontModule.php
@@ -5,18 +5,5 @@ namespace fecshop\app\appfront\modules;
 // Appfront 入口的Module的基类
 class AppfrontModule extends \yii\base\Module
 {
-    /*
-    public function init()
-    {
-        # 以下代码必须指定
-        parent::init();
-        //$this->_currentDir			= 	__DIR__ ;
-        //$this->_currentNameSpace	=   __NAMESPACE__;
-
-        # 指定默认的man文件
-        //$this->layout = "main_ajax.php";
-        parent::init();
-
-    }
-    */
+    
 }
--- app/appfront/modules/Catalog/Module.php
+++ app/appfront/modules/Catalog/Module.php
@@ -12,23 +12,18 @@ class Module extends AppfrontModule
     public function init()
     {
         parent::init();
-        // 以下代码必须指定
-        $nameSpace = __NAMESPACE__;
-        // 如果 Yii::$app 对象是由类\yii\web\Application 实例化出来的。
-        if (Yii::$app instanceof \yii\web\Application) {
-            // 设置模块 controller namespace的文件路径
-            $this->controllerNamespace = $nameSpace . '\\controllers';
-            // 设置模块block namespace的文件路径
-            $this->blockNamespace = $nameSpace . '\\block';
-        // console controller
-        //} elseif (Yii::$app instanceof \yii\console\Application) {
-        //	$this->controllerNamespace 	= 	$nameSpace . '\\console\\controllers';
-        //	$this->blockNamespace 	= 	$nameSpace . '\\console\\block';
-        }
-        //$this->_currentDir			= 	__DIR__ ;
-        //$this->_currentNameSpace	=   __NAMESPACE__;
 
-        // Set the default theme layout file of this current module.
-        Yii::$service->page->theme->layoutFile = 'home.php';
+        // 获取当前路径（fecshop\app\appfront\modules\Catalog）
+        $nameSpace = __NAMESPACE__;
+        
+        // 判断 Yii::$app 对象是由类\yii\web\Application 实例化出来的。
+        if (Yii::$app instanceof \yii\web\Application) {
+            // 设置模块 controller block 的文件路径
+            $this->controllerNamespace = $nameSpace . '\\controllers';
+            $this->blockNamespace = $nameSpace . '\\block';
+        }
+
+        // 设置本模块theme的默认layout文件。
+        Yii::$service->page->theme->layoutFile = 'main.php';
     }
 }
